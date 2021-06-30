96f7996463c824463421ccf3b6d58c6851ef2aec
bitngu <biinguyen@ucdavis.edu>
2021-06-30 06:34:55 -0700

    update links to social site

--- src/View/Footer.jsx
+++ src/View/Footer.jsx
@@ -9,14 +9,16 @@ const Footer = () => {
         <footer>
             <div className = 'footer-content'>
             <div className="socialnetwork" id = 'network-footer'> 
-                    <div id = 'rotate-logo1' className="github">
+                    <div onClick= {() => window.open('https://github.com/bitngu') } id = 'rotate-logo1' className="github">
                         <img src={github} alt= "github-logo" />
                     </div>
-                    <div id = 'rotate-logo2' className="linkedin">
+                    <div onClick= {() => window.open('https://www.linkedin.com/in/bitngu/')} id = 'rotate-logo2' className="linkedin">
                         <img src={linkedin} alt= "linkedIn-logo" />
                     </div>
                     <div id = 'rotate-logo3' className="gmail">
-                        <img src={gmail} alt= "gmail-logo" />
+                        <a href = "mailto: bitngu3@gmail.com">
+                            <img src={gmail} alt= "gmail-logo" />
+                        </a>
                     </div>
                 </div>
                 <h3> &copy; 2021 Bi Nguyen. All Rights Reserved</h3>
