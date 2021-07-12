73d724fbbc1700ef9a20adc43d1721fdb4a74813
bitngu <biinguyen@ucdavis.edu>
2021-07-11 23:44:50 -0700

    change view pic

--- src/View/About.jsx
+++ src/View/About.jsx
@@ -23,6 +23,11 @@ const About = () => {
                         <h1>Hello!</h1> 
                         <h1>I'm <span>Bi Nguyen.</span></h1>
                     </div>
+                    {/* I am an aspiring Software Engineer. I took an interest in Computer Science while studying Statistics 
+                    at UC Davis. I enjoy the creativity that revolves around programming. With programming, I can develop games,
+                     websites and solve problems. The versality of coding allows you to get creative because there is no one way 
+                     to solve a problem. I have been trying to improve my skills and knowledge in programming by taking the
+                      upper division Computer Science courses offered at UC Davis and taking online classes offered at Udemy. */}
 
                     <p>I am an aspiring Software Engineer. I took an interest in Computer Science while studying Statistics at UC Davis.
                     I remember when I first took my Intro to Programming course, I thought it was the hardest thing. 
